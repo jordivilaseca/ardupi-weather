075de577cb57087fea434438d233af8532c7710e
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-05-02 19:36:32 +0200

    Added DHTxx support

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,28 +1,36 @@
-// temps entre actualitzacions de els elements
-// [lm35_1, lm35_2, ldr_1]
 #include <Wire.h>
 #include <SFE_BMP180.h>
+#include "DHT.h"
 
 #define count(x) sizeof(x)/sizeof(unsigned long)
 #define groundPin A0
-#define ALTITUDE 570
+
+
+#define ALTITUDE 570 // We need it for the barometric pressure calculus
 
 #define BMP180T 0   // Temperature
 #define BMP180P 1   // Pressure
+#define DHT22T  2   // Temperature
+#define DHT22H  3   // Humidity
+#define DHT22HI 4   // Heat Index
 
-#define NOTUSED -1  // For BMP180 sensor.
+#define NOTUSED -1  // For BMP180/DHT22 sensor.
+
+#define DHTTYPE DHT22
+#define DHTPIN  4
 
 SFE_BMP180 pressure;
+DHT dht(DHTPIN, DHTTYPE);
 
-int types[] = {BMP180T,BMP180P};
-unsigned long lastUpdate[] = {0,0};
-unsigned long updateTimes[] = {30000,60000};
-int pins[] = {NOTUSED,NOTUSED};
+int types[] = {BMP180T,BMP180P,DHT22T,DHT22H,DHT22HI};
+unsigned long lastUpdate[] = {0,0,0,0,0};
+unsigned long updateTimes[] = {30000,60000,30000,30000,60000};
+int pins[] = {NOTUSED,NOTUSED,NOTUSED,NOTUSED,NOTUSED};
 
 int secureAnalogRead(int pin) {
-  delay(0.01);
-  analogRead(groundPin);
-  delay(0.01);
+  //delay(0.01);
+  //analogRead(groundPin);
+  //delay(0.01);
   return analogRead(pin);
   
 }
@@ -43,7 +51,6 @@ boolean bmp180Pressure(double &P) {
   char status;
   if(bmp180Temperature(T)) {
     Serial.println(T);
-    delay(500);
     status = pressure.startPressure(3);
     if (status != 0) {
         delay(status);
@@ -74,17 +81,48 @@ void update(int type, int pin) {
   String c, v;
   switch (type) {
     case BMP180T:
-        double T;
-        c = "BMP180T";
-        if (bmp180Temperature(T)) v = String(T);
-        else v = c + " Error";
-        break;
+      {
+      double t;
+      c = "BMP180T";
+      if (bmp180Temperature(t)) v = String(t);
+      else v = c + " Error";
+      break;
+      }
+
     case BMP180P:
-        double P;
-        c = "BMP180P";
-        if (bmp180Pressure(P)) v = String(P);
-        else v = c + " Error";
-        break;
+      {
+      double p;
+      c = "BMP180P";
+      if (bmp180Pressure(p)) v = String(p);
+      else v = c + " Error";
+      break;
+      }
+
+    case DHT22T:
+      {
+      float t = dht.readTemperature();    // Default temperature unit is ºC.
+      c = "DHT22T";
+      v = String(t);
+      break;
+      }
+
+    case DHT22H:
+      {
+      float h = dht.readHumidity();
+      c = "DHT22H";
+      v = String(h);
+      break;
+      }
+    case DHT22HI:
+      {
+      float h, t, hi;
+      h = dht.readHumidity();
+      t = dht.readTemperature(true);    // We need value in farenheit.
+      hi = dht.computeHeatIndex(t,h);
+      c = "DHT22HI";
+      v = String(dht.convertFtoC(hi));
+      break;
+      }
   }
   sendCommand(c, v);
 }
@@ -138,6 +176,7 @@ void setup() {
   Serial.begin(9600); // set the baud rate
   if (pressure.begin()) Serial.println("BMP180 init success");
   else Serial.println("BMP180 init fail\n\n");
+  dht.begin();
   
   for (int i = 0; i < count(updateTimes); i++) {
     pinMode(pins[i], INPUT);
