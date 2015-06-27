cd9afd6535e9d16e9d8e69572bdca853bd9112f7
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-06-27 11:12:11 +0200

    Added mongodb support

--- mongodb.py
+++ mongodb.py
@@ -22,6 +22,31 @@ class mongodb():
 		except pymongo.errors.ConnectionFailure as e:
 			print ("Could not connect to MongoDB: %s" % e)
 
+	def getCollection(self, dbName, dbCollection):
+		return self.client[dbName][dbCollection]
+
+	def insert(self, dbName, dbCollection, dic):
+		coll = self.getCollection(dbName,dbCollection)
+		coll.insert(dic)
+
+	def queryOne(self, dbName, dbCollection, queryDic=None):
+		coll = self.getCollection(dbName,dbCollection)
+		return list(coll.find_one(queryDic))
+
+	def query(self, dbName, dbCollection, queryDic=None):
+		coll = self.getCollection(dbName,dbCollection)
+		return list(coll.find(queryDic))
+
+	def queryBetweenValues(self, dbName, dbCollection, attribute, minValue, maxValue):
+		coll = self.getCollection(dbName,dbCollection)
+		return list(coll.find({attribute: {"$gte": minValue, "$lte": maxValue}}))
+
+	def queryBetweenValuesSortAsc(self,dbName,dbCollection,attribute,minValue,maxValue):
+		coll = self.getCollection(dbName,dbCollection)
+		return list(coll.find({attribute: {"$gte": minValue, "$lte": maxValue}}).sort([(attribute, pymongo.ASCENDING)]))
+
 if __name__ == '__main__':
 	db = mongodb()
-	print(db.client.database_names())
+	ret = db.queryBetweenValuesSortAsc('prova','prova1', 'pepe', 'aaaaa','ccccc')
+	for elem in ret:
+		print (elem)
