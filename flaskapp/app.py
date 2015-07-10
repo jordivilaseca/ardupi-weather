aa9b6f516ce36aab99d2d02f1a6c0fb812e9ff21
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-07-10 17:34:49 +0200

    Changed variables passed to template

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -21,14 +21,8 @@ def readConfiguration():
 		cfg = dict(ymlData['webserver'])
 	return dict(cfg)
 
-def createChart():
-	pass
-
-@app.route('/')
-def home(chartID = 'chart_ID', chart_height = 700):
-	cfg = readConfiguration()
-	chart = {"height": chart_height}
-	rangeSelector = {"selected": 1}
+def createChart(cfg):
+	chart = {}
 	series = []
 	yAxis = []
 	panels = cfg['charts']['history']['panels']
@@ -42,9 +36,21 @@ def home(chartID = 'chart_ID', chart_height = 700):
 		yAxis.append({'title': {'text': panel['name']}, 'height': str(height)+'%', 'top': str(top)+'%', 'offset': 0, 'labels': {'align': 'right', 'x': -3}})
 		top += height
 		axisNum += 1
-	title = {"text": 'My Title'}
-	xAxis = {"categories": ['Time']}
-	return render_template('index.html', image_name='static/img/header.jpg', rangeSelector=rangeSelector, chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+
+	chart['chart'] = {"height": 700}
+	chart['chartID'] = 'chart_ID'
+	chart['rangeSelector'] = {"selected": 1}
+	chart['series'] = series
+	chart['title'] = {"text": 'My Title'}
+	chart['xAxis'] = {"categories": ['Time']}
+	chart['yAxis'] = yAxis
+	return chart
+
+@app.route('/')
+def home():
+	cfg = readConfiguration()
+	chart = createChart(cfg)
+	return render_template('index.html', image_name='static/img/header.jpg', chart=chart)
  
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, passthrough_errors=True)
