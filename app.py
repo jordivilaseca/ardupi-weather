18f8f2041d5301cde77a70655898f1b1bfbd3b19
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-07-12 17:15:38 +0200

    Added host and port configuration

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def createChart():
 	series = []
 	yAxis = []
 	panels = cfg['webserver']['charts']['history']['panels']
-	height = 100/len(panels)
 	top = 0
 	axisNum = 0
 	for panel in panels:
@@ -25,11 +24,11 @@ def createChart():
 			data = readJsonData(dataPath + f + '.json')
 			tooltip = {'valueSuffix' : ' ' + panel['units']}
 			series.append({'type':panel['type'],'name':f,'data': data, 'yAxis': axisNum, 'tooltip' : tooltip})
-		yAxis.append({'title': {'text': panel['name']}, 'height': str(height)+'%', 'top': str(top)+'%', 'offset': 0, 'labels': {'align': 'right', 'x': -3}})
-		top += height
+		yAxis.append({'title': {'text': panel['name']}, 'height': str(panel['height'])+'%', 'top': str(top)+'%', 'offset': 0, 'labels': {'align': 'right', 'x': -3}})
+		top += panel['height'] + panel['offset']
 		axisNum += 1
 
-	chart['chart'] = {"height": 700}
+	chart['chart'] = {"height": cfg['webserver']['charts']['history']['height']}
 	chart['chartID'] = 'chart_ID'
 	chart['rangeSelector'] = {"selected": 1}
 	chart['series'] = series
@@ -44,4 +43,4 @@ def home():
 	return render_template('index.html', image_name='static/img/header.jpg', chart=chart)
  
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug = True)
+	app.run(host=cfg['webserver']['host'], port=cfg['webserver']['port'], debug = True)
